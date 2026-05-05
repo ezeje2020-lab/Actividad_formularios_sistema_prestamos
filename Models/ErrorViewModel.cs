1ad2bf5614e7f942c6b0ada71024064ca3f5b600
URIEL MATIAS UNER <50102164@ort>
2026-05-05 09:02:48 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ActividadFormularios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
