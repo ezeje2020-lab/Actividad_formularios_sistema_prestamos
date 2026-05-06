5e21f69d5cc09aa5b9eb2a31bc30b9a9e41f2efb
URIEL MATIAS UNER <50102164@ort>
2026-05-06 08:57:33 -0300

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,8 +13,19 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult GuardarInfo(string nombre, int edad, int DNI, ){
-
+    public IActionResult GuardarInfo(string nombre, int edad, int DNI, bool empleo, string tipoEmpleo, double ingresoMensual, bool deuda, string tipoDeuda, double montoSolicitado, string plazoDevolucion, bool acepto){
+        string text1;
+        string text2;
+        ViewBag.nombre = nombre;
+        if(edad<18 || !empleo || ingresoMensual<250000 || montoSolicitado>ingresoMensual/5 || deuda || !acepto){
+            ViewBag.text1 = "Lo sentimos,";
+            ViewBag.text2 = "no cumple con los requisitos";
+        }
+        else{
+            ViewBag.text1 = "Felicidades,";
+            ViewBag.text2 = "! Su préstamo ha sido realizado y le será enviado a la brevedad";
+        }
+        return View("Request");
     }
 
     public IActionResult Index()
